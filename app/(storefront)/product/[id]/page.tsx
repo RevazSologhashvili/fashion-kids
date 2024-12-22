5d8f536a58e6300c6156385ab4f8912a8e002e48
RevazSologhashvili <revaz.sologhashvili@gmail.com>
2024-12-22 18:15:09 +0400

    fixed Eslint error in typo

--- app/(storefront)/product/[id]/page.tsx
+++ app/(storefront)/product/[id]/page.tsx
@@ -7,7 +7,6 @@ import { StarIcon } from "lucide-react";
 import { notFound } from "next/navigation";
 
 async function getData(productId: string) {
-
   const data = await prisma.product.findUnique({
     where: {
       id: productId,
