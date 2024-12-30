f12e8795215304a0ddcbb31a385ea2a2162f31e4
Revaz Sologhashvili <rsologhashvili@evex.ge>
2024-12-30 13:03:01 +0400

    added some changes

--- app/lib/redis.ts
+++ app/lib/redis.ts
@@ -1,7 +1,7 @@
 import {Redis} from '@upstash/redis'
 
 export const redis = new Redis({
-    url: process.env.REDIS_URL,
-    token: process.env.REDIS_TOKEN
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN
 })
 
